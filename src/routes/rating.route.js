14be8afcd5dc1f312d1fc1a1d4e012379e66e022
Roney <roney@NDK36-roney-mac.local>
2024-09-19 17:56:29 +0300

    Auth Verification

--- src/routes/rating.route.js
+++ src/routes/rating.route.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const ratingController = require('../controllers/rating.controller');
+const verifyToken = require('../middlewheres/verifyToken');
 const router = express.Router();
 
 // Route for submitting ratings
 router.post('/:entityType/:entityId', ratingController.submitRating);
 // Route for retrieving ratings
-router.get('/:entityType/:entityId', ratingController.getRatings);
+router.get('/:entityType/:entityId',verifyToken(['admin']), ratingController.getRatings);
 
 
 module.exports = router;
