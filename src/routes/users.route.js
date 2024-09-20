635a44fc2b45a94c85f4d59d57addbcd1fe31bf6
Roney <roney@NDK36-roney-mac.local>
2024-09-20 15:02:41 +0300

    Restrict Admin Endpoints

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -2,15 +2,17 @@ const express = require('express');
 
 const router = express.Router();
 const userController = require("../controllers/users.controller");
+const verifyToken = require("../middlewheres/verifyToken");
 
+// router.use(verifyToken)
 
-router.get('/',userController.getAllUsers)
-router.post('/create', userController.createUser)
+router.get('/',verifyToken(),userController.getAllUsers)
+router.post('/create',verifyToken(), userController.createUser)
 router.post('/login', userController.login)
-router.patch('/update/:id',userController.updateAdminUser)
-router.delete('/:id', userController.getAdminUser)
-router.patch('/delete/:id', userController.deleteAdminUser)
-router.post('/reset-password', userController.resetPassword);
+router.patch('/update/:id',verifyToken(),userController.updateAdminUser)
+router.delete('/:id',verifyToken(), userController.getAdminUser)
+router.patch('/delete/:id',verifyToken(), userController.deleteAdminUser)
+router.post('/reset-password',verifyToken(), userController.resetPassword);
 
 
 
